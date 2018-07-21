87e06ed22e38d2fa746c5b785373707b60afe095
canvas-j <1341281680@qq.com>
2018-07-22 00:39:19 +0800

    头部代理中间件，各大搜索引擎以及其它好用的头部

--- taobao_spider/taobao_spider/MidWare/HeaderMidWare.py
+++ taobao_spider/taobao_spider/MidWare/HeaderMidWare.py
@@ -1,22 +1,14 @@
-# encoding: utf-8
-from scrapy.utils.project import get_project_settings
-from fake_useragent import UserAgent
+# -*- encoding: utf-8 -*-
+import scrapy
 import random
 
-settings = get_project_settings()
-
 class ProcessHeaderMidware():
-    """process request add request info"""
-
+    
     def process_request(self, request, spider):
-        """
-        随机从列表中获得header， 并传给user_agent进行使用
-        """
-        ua = UserAgent().chrome
-        # ua = random.choice(settings.get('USER_AGENT_LIST'))
+        useragent = random.choice(scrapy.crawler.Settings.get('USER_AGENT_LIST'))
         spider.logger.info(msg='now entring download midware')
-        if ua:
-            request.headers['User-Agent'] = ua
+        if useragent:
+            request.headers['User-Agent'] = useragent
             # Add desired logging message here.
             spider.logger.info('User-Agent is : {} {}'.format(request.headers.get('User-Agent'), request))
         pass
