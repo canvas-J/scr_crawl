8965507e91e8833b45d13ecd856a6d8c994aab78
canvas-j <1341281680@qq.com>
2018-07-29 23:16:09 +0800

    解决或许信息缺失问题(解析规则)

--- atobourl/atobourl/spiders/clinks.py
+++ atobourl/atobourl/spiders/clinks.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import scrapy, time
 from atobourl.items import AtobourlItem
 
 
@@ -9,28 +9,28 @@ class ClinksSpider(scrapy.Spider):
     start_urls = ['https://www.atobo.com.cn/Companys/']
 
     def parse(self, response):
-        global category
         num = 0
         for each_url in response.xpath('//li[contains(@id, "filter")]'):
             num += 1
             if num > 8:
-                category = each_url.xpath('.//a/text()').extract_first()
                 links = 'https://www.atobo.com.cn' + each_url.xpath('.//a/@href').extract_first()
                 yield scrapy.Request(url=links, callback=self.parse_item)
 
     def parse_item(self, response):
         for company in response.xpath('//li[@class="product_box"]'):
             item = AtobourlItem()
-            item['category'] = category
-            item['co_links'] = "https" + company.xpath('.//a[@class="CompanyName"]/@href').extract_first()
+            item['category'] = response.xpath('//div[@class="filterMode"]/h2/strong/text()').extract_first()
+            item['co_links'] = "https:" + company.xpath('.//a[@class="CompanyName"]/@href').extract_first()
             g_url = company.xpath('.//li[@class="pp_name"]/a[3]/@href').extract_first()
-            if g_url is not None:
-                item['bu_links'] = "https" + str(g_url)
+            if g_url != None:
+                item['bu_links'] = "https:" + str(g_url)
+            elif company.xpath('.//li[@class="pp_name"]/a[2]/@href').extract_first() != None:
+                item['bu_links'] = "https:" + str(company.xpath('.//li[@class="pp_name"]/a[2]/@href').extract_first())
             else:
                 item['bu_links'] = ""
             item['c_name'] = company.xpath('.//a[@class="CompanyName"]/text()').extract_first()
-            item['c_product'] = company.xpath('string(.//li[@class="pp_product"]/strong)').extract()
-            item['c_address'] = company.xpath('string(.//li[@class="pp_address"]/strong)').extract()
+            item['c_product'] = company.xpath('string(.//li[@class="pp_product"])').extract()
+            item['c_address'] = company.xpath('string(.//li[@class="pp_address"])').extract()
             yield item
 
             if '-y' not in response.url:
